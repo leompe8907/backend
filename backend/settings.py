6d8d82e2a8b5ebcfa60ad1bdf8bb95029c7e9c9c
leompe8907 <veyqpasa1312@gmail.com>
2024-07-25 14:23:06 -0300

    Add models for Telemetria and related tables and configure DATABASES

--- backend/settings.py
+++ backend/settings.py
@@ -81,17 +81,17 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 #     }
 # }
 
+# Configuración de la base de datos
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
-# DATABASES sqlite3
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": "mydatabase",
-#     }
-# }
+# Configuración de la base de datos para producción
+if 'DATABASE_URL' in os.environ:
+    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
 # Password validation
