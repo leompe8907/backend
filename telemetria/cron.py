e3c63394aaeebcee87d35497e00bc800896c8400
leompe8907 <veyqpasa1312@gmail.com>
2024-07-29 07:39:42 -0300

    modificacion del horario de actualizacion de la base de datos

--- telemetria/cron.py
+++ telemetria/cron.py
@@ -2,53 +2,53 @@ from django_cron import CronJobBase, Schedule
 from .tasks import test_fetch_store_telemetry, update_data_ott, update_data_dvb, update_data_end_catchup, update_data_stop_vod, update_data_end_vod
 
 class TestFetchStoreTelemetryCronJob(CronJobBase):
-    RUN_EVERY_MINS = 12 * 60  # Cada 12 horas
-    schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
+    RUN_AT_TIMES = ['00:00', '12:00']  # Se ejecuta a las 00:00 y 12:00
+    schedule = Schedule(run_at_times=RUN_AT_TIMES)
     code = 'telemetria.test_fetch_store_telemetry'  # un código único
 
     def do(self):
         test_fetch_store_telemetry()
 
 class UpdateDataOttCronJob(CronJobBase):
-    RUN_EVERY_MINS = 40
+    RUN_AT_TIMES = ['00:00', '12:00']
 
-    schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
+    schedule = Schedule(run_at_times=RUN_AT_TIMES)
     code = 'telemetria.update_data_ott'
 
     def do(self):
         update_data_ott()
 
 class UpdateDataDvbCronJob(CronJobBase):
-    RUN_EVERY_MINS = 40
+    RUN_AT_TIMES = ['00:00', '12:00']
 
-    schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
+    schedule = Schedule(run_at_times=RUN_AT_TIMES)
     code = 'telemetria.update_data_dvb'
 
     def do(self):
         update_data_dvb()
 
 class UpdateDataEndCatchupCronJob(CronJobBase):
-    RUN_EVERY_MINS = 40
+    RUN_AT_TIMES = ['00:00', '12:00']
 
-    schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
+    schedule = Schedule(run_at_times=RUN_AT_TIMES)
     code = 'telemetria.update_data_end_catchup'
 
     def do(self):
         update_data_end_catchup()
 
 class UpdateDataStopVodCronJob(CronJobBase):
-    RUN_EVERY_MINS = 40
+    RUN_AT_TIMES = ['00:00', '12:00']
 
-    schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
+    schedule = Schedule(run_at_times=RUN_AT_TIMES)
     code = 'telemetria.update_data_stop_vod'
 
     def do(self):
         update_data_stop_vod()
 
 class UpdateDataEndVodCronJob(CronJobBase):
-    RUN_EVERY_MINS = 40
+    RUN_AT_TIMES = ['00:00', '12:00']
 
-    schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
+    schedule = Schedule(run_at_times=RUN_AT_TIMES)
     code = 'telemetria.update_data_end_vod'
 
     def do(self):
